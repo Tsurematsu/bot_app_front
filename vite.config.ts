0333936f6eae92700726e3aae96ce2d6accf5888
Tsurematsu <daniel120802@gmail.com>
2025-10-08 17:52:10 -0500

    configuraron

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base:'/',
   build:{
-    outDir:'dist',
-    assetsDir:'public',
-    copyPublicDir: true
+    outDir:'dist'
   },
   server: {
     host: true,       // Escucha en 0.0.0.0 (todas las IPs disponibles)
     port: 5173,       // Puedes cambiar el puerto si lo deseas
-  }
+  },
+  plugins: [
+    {
+      name: 'replace-public-paths',
+      enforce: 'pre',
+      transform(code, id) {
+        if (/\.(js|ts|vue|html|png|ico|jpg)$/.test(id)) {
+          return code.replace(/\/public\//g, '/')
+        }
+      }
+    }
+  ]
 })
