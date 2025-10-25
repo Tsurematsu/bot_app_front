444fa10a8f1622aae37d26c6174e52bdf36d8edb
Tsurematsu <daniel120802@gmail.com>
2025-10-25 01:31:29 -0500

    fix

--- src/Request/Script.ts
+++ src/Request/Script.ts
@@ -2,7 +2,7 @@ import FETCH from "../tools/FETCH";
 
 export  default class  Script{
     public static  async request(email, panelStatus){
-        const url = window.location.origin.includes("localhost") ? "https://example.com" : `${window.location.origin}/request`;   
+        const url = window.location.origin.includes("localhost") ? "https://example.com" : `${window.location.origin}/?request`;   
         try {
             const data = {
                 "email": email,
