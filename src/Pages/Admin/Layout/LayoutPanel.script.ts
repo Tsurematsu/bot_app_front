77d355b6df3b95dce80d9d4f18db02dadb16bd9a
Tsurematsu <daniel120802@gmail.com>
2025-10-27 22:00:30 -0500

    update

--- src/Pages/Admin/Layout/LayoutPanel.script.ts
+++ src/Pages/Admin/Layout/LayoutPanel.script.ts
@@ -4,28 +4,28 @@ import Fetch from "../../../Helpers/herlperFetch";
 
 export default class StartPanelScript{
     public static async onValidUser(setStatus, statusInstant){
+        const res = await Fetch.get("/admin")
+        console.log(res);
+        
+        return
         const cache = getStorage('access')
         if (cache && cache=="admin") {
             statusInstant(true)
             const res = await Fetch.get("/admin")
-            console.log(res);
-            
-            // if (res.success) return 
-            // deleteStorage('access')
-            // setStatus(false)
-            // return Router.go("/admin")
+            if (res.success) return 
+            deleteStorage('access')
+            setStatus(false)
+            return Router.go("/admin")
         }
         if(!cache){
             statusInstant(false)
             const res = await Fetch.get("/admin")
-            console.log(res);
-            
-            // if (res.success) {
-            //     setStorage("access", "admin")
-            //     setStatus(true)
-            //     return   
-            // }  
-            // return Router.go("/admin")
+            if (res.success) {
+                setStorage("access", "admin")
+                setStatus(true)
+                return   
+            }  
+            return Router.go("/admin")
         }
         return Router.go("/")
     }
