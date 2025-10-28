2e835185bac094f89ef5a673c1f0f1ae33664560
Tsurematsu <daniel120802@gmail.com>
2025-10-27 22:05:54 -0500

    creal local storage

--- src/Pages/Home/page-home.ts
+++ src/Pages/Home/page-home.ts
@@ -14,6 +14,7 @@ export class HOme extends LitElement {
         window.open(msg, '_blank').focus();
     }
     render() {
+        localStorage.clear();
         return html`
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
             <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet"/>
